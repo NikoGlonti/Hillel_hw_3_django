619e8212cdc3e9e8a22e6d5fa6ff09c6c2496417
”NikoGlonti” <niko.guria@gmail.com>
2021-12-10 23:01:24 +0200

    upgrade

---
+++ mysite/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
+
+
+
+
